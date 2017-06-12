8e9c998dac158654f71125d1e8971bbb1b2e0c77
Remco <r.m.van.buijtenen@student.rug.nl>
2017-06-12 17:20:59 +0200

    added comments to arrayDataManager

--- src/js/datamanagers/arrayDataManager.js
+++ src/js/datamanagers/arrayDataManager.js
@@ -1,16 +1,28 @@
+/*
+ * The ArrayHandler acts as a protective layer between javascripts Array class
+ * and ManagedData. This is neccesary to preserve array.push(item) syntax while performing
+ * type checking against the schema.
+ */
 export class ArrayHandler {
 	constructor(items) {
 		this.items = items;
 	}
 
 	get(target, propKey, receiver) {
+		/* We only want to modify the behaviour of push when there are restrictions defined in items */
 		if(propKey == 'push' && this.items.length > 0) {
 			const origMethod = target[propKey];
 			const items = this.items;
+			/*
+			 * Return a function that performs the type validation and then pushes the item on
+			 * the target array if the check was succesful.
+			 */
 			return function (...args) {
+				/* for each argument: */
 				for(let arg of args) {
 					let includes = false;
 
+					/* Check if the type of the argument is included in the items property */
 					let type = ArrayHandler.getType(arg);
 					for(let item of items) {
 						if(item.hasOwnProperty("enum")) {
@@ -26,6 +38,7 @@ export class ArrayHandler {
 						}
 					}
 
+					/* if the type is not included: construct an error message and throw a typeError */
 					if(!includes) {
 						let msg = "["
 						for(let item of items) {
@@ -42,13 +55,16 @@ export class ArrayHandler {
 						throw new TypeError("items in this array must be one of " + msg);
 					}
 				}
+				/* At this point validation was succesful, so we apply the originally called method and return the result */
 	            let result = origMethod.apply(this, args);
 	            return result;
 	        };
 		}
+		/* Another method than push was invoked, so we simply forward the call to the target using Reflect */
 		return Reflect.get(target, propKey, receiver);
 	}
 
+	/* A method that returns a string describing the type of an item */
 	static getType(item) {
 		if(item.hasOwnProperty('klass')) {
 			return item.klass;
